ff5354fbc0c4627f4e7a3bda9083c9940afd21c7
illuminosity08 <174038102+Illuminosity08@users.noreply.github.com>
2024-07-17 19:57:12 -0400

    j

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,11 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.swervedrive.Swerve;
+
+import java.io.File;
+
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -21,7 +25,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final Swerve m_exampleSubsystem = new Swerve();
+  private final Swerve m_exampleSubsystem = new Swerve((new File(Filesystem.getDeployDirectory(),
+  "swerve")));
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController = new CommandXboxController(
